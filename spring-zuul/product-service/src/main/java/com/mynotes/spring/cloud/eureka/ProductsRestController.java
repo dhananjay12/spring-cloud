29b127393daf41f601974c22eaddcf73fba0c2f7
Dhananjay Singh <dhananjay1204@gmail.com>
2016-12-29 23:40:34 +0530

    spring-zuul demo

---
+++ spring-zuul/product-service/src/main/java/com/mynotes/spring/cloud/eureka/ProductsRestController.java
@@ -0,0 +1,19 @@
+package com.mynotes.spring.cloud.eureka;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductsRestController {
+	
+	@RequestMapping(value = "/categories", method = RequestMethod.GET)
+	@ResponseBody
+	public String getContact() {
+		return "Product Categories";
+	}
+	
+
+}
