a3ff0f40cd59866d46a91b5ab097f07387fa0bd7
Dhananjay Singh <dhananjay1204@gmail.com>
2019-02-02 23:36:42 +0530

    Configuring properties

---
+++ spring-config/configuring-properties/src/main/java/com/mynotes/spring/boot/configuringproperties/ConfiguringPropertiesApplication.java
@@ -0,0 +1,14 @@
+package com.mynotes.spring.boot.configuringproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguringPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguringPropertiesApplication.class, args);
+	}
+
+}
+
