bb6a1113429b65ed894557ad0803298cbc97d112
Dhananjay Singh <dhananjay1204@gmail.com>
2019-02-10 01:58:09 +0530

    spring cloud config

--- spring-config/config-server-demo/src/main/java/com/mynotes/spring/cloud/config/Application.java
+++ spring-config/config-server-demo/src/main/java/com/mynotes/spring/cloud/config/Application.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
