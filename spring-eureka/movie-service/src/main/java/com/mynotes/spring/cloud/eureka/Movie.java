49574893daf68c44fe92966f9ddedb2108b47795
Dhananjay Singh <dhananjay1204@gmail.com>
2019-02-14 21:18:52 +0530

    movie controller

---
+++ spring-eureka/movie-service/src/main/java/com/mynotes/spring/cloud/eureka/Movie.java
@@ -0,0 +1,30 @@
+package com.mynotes.spring.cloud.eureka;
+
+public class Movie {
+
+    private Integer id;
+
+    private String name;
+
+    private String synopsis;
+
+    public Movie(Integer id, String name, String synopsis) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.synopsis = synopsis;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+}
--- spring-eureka/movie-service/src/main/java/com/mynotes/spring/cloud/eureka/MovieController.java
+++ spring-eureka/movie-service/src/main/java/com/mynotes/spring/cloud/eureka/MovieController.java
@@ -1,19 +1,49 @@
 package com.mynotes.spring.cloud.eureka;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
 public class MovieController {
-	
-	@RequestMapping(value = "/getPublicMailingAddress", method = RequestMethod.GET)
-	@ResponseBody
-	public String getContact() {
-		return "Public mail Address";
-	}
-	
+
+    private static List<Movie> movieList = new ArrayList<>();
+    static {
+        movieList.add(new Movie(1, "movie-1", "summary-1"));
+        movieList.add(new Movie(2, "movie-2", "summary-2"));
+        movieList.add(new Movie(3, "movie-3", "summary-3"));
+    }
+
+    @GetMapping("/movies")
+    public ResponseEntity<?> getMovies() {
+
+        return ResponseEntity.ok(movieList);
+
+    }
+
+    @GetMapping("/movie/{id}")
+    public ResponseEntity<?> getMovie(@PathVariable int id) {
+
+        Movie movie = findMovie(id);
+        if (movie == null) {
+            return ResponseEntity.badRequest()
+                .body("Invalid movie id");
+        }
+
+        return ResponseEntity.ok(movie);
+
+    }
+
+    private Movie findMovie(int id) {
+        return movieList.stream()
+            .filter(movie -> movie.getId()
+                .equals(id))
+            .findFirst()
+            .orElse(null);
+    }
 
 }
