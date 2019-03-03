6f9a74dfcf32b6e2168824ba45962d9430c602aa
Dhananjay Singh <dhananjay1204@gmail.com>
2019-03-03 17:48:19 +0100

    Spring eureka

--- spring-eureka/recommendation-service/src/main/java/com/mynotes/spring/cloud/eureka/Application.java
+++ spring-eureka/recommendation-service/src/main/java/com/mynotes/spring/cloud/eureka/Application.java
@@ -3,13 +3,22 @@ package com.mynotes.spring.cloud.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
--- spring-eureka/recommendation-service/src/main/java/com/mynotes/spring/cloud/eureka/RecommendationController.java
+++ spring-eureka/recommendation-service/src/main/java/com/mynotes/spring/cloud/eureka/RecommendationController.java
@@ -1,19 +1,22 @@
 package com.mynotes.spring.cloud.eureka;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/users")
 public class RecommendationController {
-	
-	@RequestMapping(value = "/getPublicMailingAddress", method = RequestMethod.GET)
-	@ResponseBody
-	public String getContact() {
-		return "Public mail Address";
-	}
-	
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/recommendations", method = RequestMethod.GET)
+    @ResponseBody
+    public Movie[] recommendations() {
+        Movie[] result = restTemplate.getForObject("http://movie-service/movies", Movie[].class);
+        return result;
+    }
 }
