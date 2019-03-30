4cca5e071e6783faad14ebbe50de03af68f710b4
Dhananjay Singh <dhananjay1204@gmail.com>
2019-03-30 22:28:08 +0100

    Spring cloud gateway

---
+++ spring-routing/gateway-edge-server/src/test/java/com/mynotes/spring/cloud/gateway/gatewayedgeserver/GatewayEdgeServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mynotes.spring.cloud.gateway.gatewayedgeserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayEdgeServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
--- spring-routing/zuul-server/src/main/java/com/mynotes/spring/cloud/zuul/ZuulApplication.java
+++ spring-routing/zuul-server/src/main/java/com/mynotes/spring/cloud/zuul/ZuulApplication.java
@@ -8,10 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 public class ZuulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
 }
-
-
