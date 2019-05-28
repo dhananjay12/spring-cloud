9b7b806f7c2aea56bb6852d52ad0be35b5307f48
Dhananjay Singh <dhananjay1204@gmail.com>
2019-05-29 01:41:31 +0200

    Spring turbine

--- spring-turbine/hystrix-turbine/src/main/java/com/mynotes/spring/cloud/hystrix/turbine/HystrixTurbineApplication.java
+++ spring-turbine/hystrix-turbine/src/main/java/com/mynotes/spring/cloud/hystrix/turbine/HystrixTurbineApplication.java
@@ -2,13 +2,13 @@ package com.mynotes.spring.cloud.hystrix.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHystrixDashboard
 public class HystrixTurbineApplication {
 
